0e75159c0bba30cc160a536f11e9627dd09c1ed3
Akira Panda <p.chenliang@gmail.com>
2014-03-22 22:56:36 +0800

    add admin

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -1,14 +1,33 @@
 class Admin::ItemsController < ApplicationController
   def index
     @items = Item.all
-    if params[:item_type].present?
-      @items = @items.where(:item_type=>params[:item_type])
-    end
+
     
     @items_grid = initialize_grid(@items,:per_page => 40)
   end
   
   def show
     @item = Item.find(params[:id])
+  end
+  
+  
+  def edit
+    @item = Item.find(params[:id])
+  end
+  
+  def update
+    @item = Item.find(params[:id])
+    
+    if @item.update(item_param)
+      redirect_to edit_admin_item_path(@item), notice: '修改成功' 
+    else
+      render 'edit'
+    end
+  end
+  
+  private 
+  
+  def item_param
+    params.require(:item).permit!
   end
 end
