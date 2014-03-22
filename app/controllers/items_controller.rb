fe3f7ce1fdcf8f42ae43038b88c6ed1ff794c3a3
Akira Panda <p.chenliang@gmail.com>
2014-03-22 23:00:13 +0800

    fix

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,12 +1,13 @@
 class ItemsController < ApplicationController
   def index
+    @q = Item.search(params[:q])
+    
     @items = Item.includes(:item_stats).includes(:item_type).where(:hidden=>false).order("level asc")
     if params[:item_type].present?
       @items = @items.where(:item_type=>params[:item_type])
     end
     @items_grid = initialize_grid(@items,:per_page => 20)
   end
-    
     
 
   def show
