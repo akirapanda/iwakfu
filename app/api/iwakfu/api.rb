d3e4bc0e26e8548b4d52ae712905ecb4d0ce4e10
Akira Panda <p.chenliang@gmail.com>
2014-04-03 14:48:32 +0800

    Add get method

--- app/api/iwakfu/api.rb
+++ app/api/iwakfu/api.rb
@@ -22,14 +22,38 @@ module Iwakfu
          present @items.includes(:item_type).order("level asc")
          
        end
+       
+       get do
+         if not params[:page].present?
+          page = 1
+         else
+          page = params[:page]
+         end
+         @items = Item.where(:hidden => false).page(page).per(10)
+         
+         if params[:type_name].present?
+            @items = @items.joins(:item_type).where(:hidden => false).where("item_types.chinese_name = ?",params[:type_name])
+         end
+         
+         present @items.includes(:item_type).order("level asc")
+         
+       end
+       
        post ":id" do
          @item = ItemDetail.where("item_id=?",params[:id])[0]
          
          present @item
        end
 
+       get ":id" do
+         @item = ItemDetail.where("item_id=?",params[:id])[0]
+         
+         present @item
+       end
+       
     end
     
+
 
     resource :item_stats do
       post ":id" do
